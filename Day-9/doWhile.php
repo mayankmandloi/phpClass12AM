2c6326c3b91201230b4c71f71dfc97e32422f281
mayankmandloi <mayankmandloi009@gmail.com>
2018-01-03 12:40:08 +0530

    Day-8

--- Day-9/doWhile.php
+++ Day-9/doWhile.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Web Bhumi while loop</title>
+    <title>Web Bhumi</title>
 </head>
 <body>
     
-<?php
 
-$i=0;
-while($i<100);
-{
-    echo "</br>$i)Web Bhumi";
-    $i=$i+10;
-}
-?>
 
+    <?php
+
+    $i=10;
+    do
+    {
+        print_r("Hi I am No.".$i++);
+
+    }while($i<10);
+
+
+    ?>
 </body>
 </html>
--- Day-9/for.php
+++ Day-9/for.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Web Bhumi while loop</title>
+    <title>Web Bhumi</title>
 </head>
 <body>
-    
+
+
 <?php
 
-$i=0;
-while($i<100);
+print_r(" Hi ");
+for($i=0;$i<10;$i++)
 {
-    echo "</br>$i)Web Bhumi";
-    $i=$i+10;
+print_r(" Bye ");
 }
-?>
-
+echo "</br>";
+echo $i;
+?>    
 </body>
 </html>
